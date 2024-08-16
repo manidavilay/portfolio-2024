5ece5fabce1845474efb444940af5af2b8576851
Manida Vilay <vilaymanida@gmail.com>
2024-08-16 14:37:22 +0200

    add data attributes

--- src/app/components/PlayMusic/PlayMusic.tsx
+++ src/app/components/PlayMusic/PlayMusic.tsx
@@ -3,18 +3,31 @@ import classNames from "classnames";
 import styles from "./styles.module.scss";
 
 interface Props {
-  isPlaying: boolean
+  isPlaying: boolean;
   setIsPlaying: (value: boolean) => void;
 }
 
 const PlayMusic = ({ isPlaying, setIsPlaying }: Props) => {
   const audioRefs = useRef<HTMLAudioElement[]>([]);
   const [volume, setVolume] = useState<number>(0.2);
-  
+
   const audioSources = [
-    "/assets/sounds/lesfm-lost-ambient-lofi-60s.mp3",
-    "/assets/sounds/purplecat-opening-the-window-for-some-fresh-air.mp3",
-    "/assets/sounds/xethrocc-summernight-lofi-vibes.mp3",
+    {
+      link: "/assets/sounds/lesfm-lost-ambient-lofi-60s.mp3",
+      source: `Music by <a href=${`https://pixabay.com/users/lesfm-22579021/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=10821`}>Oleksii Kaplunskyi</a> from <a href=${`https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=10821`}>Pixabay</a>`,
+      artist: 'Oleksii Kaplunskyi'
+    },
+    {
+      link:
+        "/assets/sounds/purplecat-opening-the-window-for-some-fresh-air.mp3",
+      source: `Music by <a href=${`https://pixabay.com/users/purrplecat-41559583/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=186916`}>Purrple Cat</a> from <a href=${`https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=186916`}>Pixabay</a>`,
+      artist: 'Purrple Cat'
+    },
+    {
+      link: "/assets/sounds/xethrocc-summernight-lofi-vibes.mp3",
+      source: `Music by <a href=${`https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=205065`}>Patrick A.</a> from <a href=${`https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=205065`}>Pixabay</a>`,
+      artist: 'Patrick A'
+    },
   ];
 
   const onAudioEnded = useCallback((index: number) => {
@@ -39,14 +52,14 @@ const PlayMusic = ({ isPlaying, setIsPlaying }: Props) => {
       setIsPlaying(true);
     }
   };
-  
+
   return (
     <>
       <div
         className={classNames(styles.playmusic__headphones, "absolute")}
         onClick={playSound}
       />
-      {audioSources.map((src, index) => (
+      {audioSources.map(({link, source, artist}, index) => (
         <audio
           key={index}
           ref={(el) => {
@@ -54,7 +67,9 @@ const PlayMusic = ({ isPlaying, setIsPlaying }: Props) => {
               audioRefs.current[index] = el;
             }
           }}
-          src={src}
+          src={link}
+          data-source={source}
+          data-artist={artist}
         />
       ))}
     </>
